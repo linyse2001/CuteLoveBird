21b08cdd8d4eefa5b7532a108267a520ca0fce12
linyse2001 <linyse2001@gmail.com>
2026-07-09 16:14:59 +0800

    Final cleanup: website files at root, remove .NET and .vs files

--- CuteLoveBird/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-// Serve all files from the "wwwroot" folder
-app.UseDefaultFiles();        // automatically looks for index.html
-app.UseStaticFiles();
-
-app.Run();
